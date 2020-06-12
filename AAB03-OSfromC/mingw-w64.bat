41444ba94bf7e692f8f0ec0cab61427d0666d7e7
FabioPlunser <57876494+FabioPlunser@users.noreply.github.com>
2020-06-12 11:05:43 +0200

    Add files via upload

---
+++ AAB03-OSfromC/mingw-w64.bat
@@ -0,0 +1,8 @@
+echo off
+rem 64 bit mingw
+set PATH=%ProgramFiles%\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\mingw64\bin;%PATH%
+
+rem 32 bit mingw from codeblocks
+rem set PATH=%ProgramFiles(x86)%\CodeBlocks\MinGW\bin;%PATH%
+
+%windir%\system32\cmd.exe
