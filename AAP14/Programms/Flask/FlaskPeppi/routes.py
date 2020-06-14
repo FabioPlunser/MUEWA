82786d6b62ebec7395b9086ac16c42f8ff9e3298
FabioPlunser <f.plunser@outlook.com>
2020-06-14 12:44:39 +0200

    pagination

--- AAP14/Programms/Flask/FlaskPeppi/routes.py
+++ AAP14/Programms/Flask/FlaskPeppi/routes.py
@@ -11,7 +11,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/")
 @app.route("/home")
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template("home.html", posts=posts)
 
 
@@ -110,7 +111,9 @@ def new_post():
         db.session.commit()
         flash("Your Post has been created!", "success")
         return redirect(url_for("home"))
-    return render_template("create_post.html", title="New Post", form=form, legend="New Post")
+    return render_template(
+        "create_post.html", title="New Post", form=form, legend="New Post"
+    )
 
 
 @app.route("/post/<int:post_id>")
@@ -127,14 +130,33 @@ def update_post(post_id):
         abort(403)
     form = PostForm()
     if form.validate_on_submit():
-        post.title = form.title.data 
-        post.content = form.content.data 
+        post.title = form.title.data
+        post.content = form.content.data
         db.session.commit()
         flash("Your Post has been updated!", "success")
         return redirect(url_for("post", post_id=post.id))
     elif request.method == "GET":
-        form.title.data = post.title 
+        form.title.data = post.title
         form.content.data = post.content
     return render_template(
         "create_post.html", title="Update Post", form=form, legend="Update Post"
     )
+
+
+@app.route("/post/<int:post_id>/delete", methods=["POST"])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash("Your post has been deleted!", "success")
+    return redirect(url_for("home"))
+
+
+@app.route("/user/<str:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template("home.html", posts=posts)
